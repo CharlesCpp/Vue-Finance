5bf1181972423b8fd1beebfd92ef6976fbdc70a0
Charles <charles.constantinesco@gmail.com>
2022-07-30 06:44:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Profile from './views/Auth.vue'
+</script>
+
+<template>
+  <div class="container">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+      <div class="row flex flex-center">
+        <RouterLink to="/profile"> Profile </RouterLink>
+        <RouterLink to="/"> Home </RouterLink>
+      </div>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,131 @@
+<template>
+  <form class="form-widget row flex flex-center" @submit.prevent="updateProfile">
+    <div>
+      <label for="email">Email</label>
+      <input id="email" type="text" :value="store.user.email" disabled />
+    </div>
+    <div>
+      <label for="username">Name</label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="website">Last name</label>
+      <input id="website" type="website" v-model="website" />
+    </div>
+
+    <div class="flex-end">
+      <input
+        type="submit"
+        class="button block primary"
+        :value="loading ? 'Loading ...' : 'Update'"
+        :disabled="loading"
+      />
+    </div>
+
+    <div class="flex-end">
+      <button class="button block" @click="signOut" :disabled="loading">
+        Sign Out
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { supabase } from "../supabase"
+import { store } from "../store"
+import { onMounted, ref } from "vue"
+
+export default {
+  setup() {
+    const loading = ref(true)
+    const username = ref("")
+    const website = ref("")
+    const avatar_url = ref("")
+
+    async function getProfile() {
+      try {
+        loading.value = true
+        store.user = supabase.auth.user()
+
+        let { data, error, status } = await supabase
+          .from("profiles")
+          .select(`id, first_name, last_name`)
+          .eq("id", store.user.id)
+          .single()
+
+        if (error && status !== 406) throw error
+
+        if (data) {
+          username.value = data.first_name
+          website.value = data.last_name
+        }
+      } catch (error) {
+        alert(error.message)
+      } finally {
+        loading.value = false
+      }
+    }
+
+    async function updateProfile() {
+      try {
+        loading.value = true
+        store.user = supabase.auth.user()
+
+        const updates = {
+          id: store.user.id,
+          first_name: username.value,
+          last_name: website.value,
+          updated_at: new Date(),
+        }
+
+        let { error } = await supabase.from("profiles").upsert(updates, {
+          returning: "minimal", // Don't return the value after inserting
+        })
+
+        if (error) throw error
+      } catch (error) {
+        alert(error.message)
+      } finally {
+        loading.value = false
+      }
+    }
+
+    async function signOut() {
+      try {
+        loading.value = true
+        let { error } = await supabase.auth.signOut()
+        if (error) throw error
+      } catch (error) {
+        alert(error.message)
+      } finally {
+        loading.value = false
+      }
+    }
+
+    onMounted(() => {
+      getProfile()
+    })
+
+    return {
+      store,
+      loading,
+      username,
+      website,
+      avatar_url,
+
+      updateProfile,
+      signOut,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.row {
+  padding: 10px;
+}
+
+.flex-end {
+  margin-top: 24px;
+}
+</style>
