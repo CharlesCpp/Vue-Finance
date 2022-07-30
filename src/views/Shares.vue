9cc17354df7fe1c26fcf621fedc3c15e2f5d55cf
Charles <charles.constantinesco@gmail.com>
2022-07-31 01:40:06 +0200

    Create home page for purchase history  Changes to be committed: 	modified:   src/style.css 	modified:   src/views/Shares.vue  Changes not staged for commit: 	modified:   src/views/Shares.vue

--- src/views/Shares.vue
+++ src/views/Shares.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-        Shares
+    <div class="container">
+         <table>
+            <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+            </tr>
+            <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+            </tr>
+            <tr>
+                <td>Centro comercial Moctezuma</td>
+                <td>Francisco Chang</td>
+                <td>Mexico</td>
+            </tr>
+        </table> 
     </div>
 </template>
 
