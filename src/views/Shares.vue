467d6df870e02a59286b2ce804d7ef3c555e4c20
Charles <charles.constantinesco@gmail.com>
2022-07-31 06:20:27 +0200

    Working on History, fixed supabase security problem  Changes to be committed: 	modified:   src/views/Shares.vue

--- src/views/Shares.vue
+++ src/views/Shares.vue
@@ -1,26 +1,66 @@
 <template>
     <div class="container">
+        <h3 v-if="loading"> Loading... </h3>
          <table>
             <tr>
-                <th>Purchase History</th>
+                <th>Title</th>
                 <th>Shares</th>
                 <th>Price</th>
             </tr>
-            <tr>
-                <td>Title</td>
-                <td>2</td>
-                <td>$25.6</td>
+            <tr v-for="row in history" :key="row.user_id">
+                <td>{{ hist.title }}</td>
+                <td>{{ hist.shares }}</td>
+                <td>{{ hist.price }}</td>
             </tr>
         </table> 
     </div>
 </template>
 
 <script>
+import { store } from "../store"
+import { onMounted, ref } from "vue"
+import { supabase } from "@/supabase"
+
 export default {
     setup () {
-        
+        const loading = ref(true);
+        const user = supabase.auth.user();
 
-        return {}
+        async function getHistory() {
+            try {
+                loading.value = true;
+                    let { data, error, status } = await supabase
+                    .from("history")
+                    .select(`user_id, title`)
+                    .eq("user_id", user.id)
+                    .single()
+
+                    if (error && status !== 406) throw error
+
+                    if (data) {
+                    this.history = data
+                    }
+                } catch (error) {
+                    alert(error.message)
+                } finally {
+                    loading.value = false
+            }
+        }
+
+        onMounted(() => {
+            getHistory()
+        })
+         
+        return {
+            loading,
+            user,
+        }
+    },
+
+    data() {
+        return {
+            history: []
+        }
     }
 }
 </script>
