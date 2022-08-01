8cc5f451dd43f80440a7fdd71b5fe817c95915c3
Charles <charles.constantinesco@gmail.com>
2022-08-01 07:33:57 +0200

    Building BuyShares page, added API requests  Changes to be committed: 	modified:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	modified:   src/views/BuyShares.vue

--- src/views/BuyShares.vue
+++ src/views/BuyShares.vue
@@ -4,10 +4,19 @@
     </div>
 </template>
 
-<script>
+<script lang="ts">
+    import axios from 'axios'
+    import { ref } from 'vue'
+    import { token } from '../stores/token'
     export default {
         setup() {
-            
+            const querry = ref(false)
+            const symbol = ref("USD")
+
+            axios.get('https://cloud.iexapis.com/stable/stock/' + symbol.value + '/quote?token=' + token.token)
+            .then(function (response) {
+                console.log(response.data.companyName)
+            })
         }
     }
 </script>
