ca9f54675863bf9ea6e4e05c164d538d67692fac
Charles <charles.constantinesco@gmail.com>
2022-08-04 22:27:43 +0200

    Building SellShares page  Changes to be committed: 	modified:   src/views/SellShares.vue

--- src/views/SellShares.vue
+++ src/views/SellShares.vue
@@ -1,8 +1,19 @@
 <template>
     <div class="container">
-        <div class="row flex flex-center" v-for="row in historyData" :key="row.id">
-        <p>{{ row.title }}</p>
-        <p>{{ row.symbol }}</p>
+        <div class="container">
+            <label for="multi-select">Your available shares: </label>
+            <div class="select">
+            <select v-model="selectValue">
+                <option disabled selected>----</option>
+                <option v-for="row in historyData" :key="row.id">{{ row.symbol }}</option>
+            </select>
+
+            <span class="focus"></span>
+            </div>
+            <div v-if="selectValue" class="Btn-Confirm form-widget  flex-center">
+                <p>How many shares you want to sell ? </p><input placeholder="Min: 1" @keypress="onlyNumber"/>
+                <button class="Btn-Confirm" @click.prevent="sellShares"> Sell</button>
+            </div>
         </div>
     </div>
 </template>
@@ -13,13 +24,15 @@ import { onMounted, ref } from "vue"
 
 export default {
     setup () {
-        const historyData = ref();
+        const historyData = ref({});
+        const selectValue = ref();
+        const sharesNumber = ref();
 
         async function getCurrentShares() {
             try {
                 let { data, error,status } = await supabase
                 .from('history')
-                .select('id, title, symbol')
+                .select('id, title, symbol, shares')
                 .eq('user_id', supabase.auth.user()?.id)
 
 
@@ -31,16 +44,52 @@ export default {
                 alert(error.message)
             }
         }
+
+        function onlyNumber ($event:any) {
+            let keyCode = ($event.keyCode ? $event.keyCode : $event.which);
+            if ((keyCode <= 48 || keyCode > 57)) { // 46 is dot
+                $event.preventDefault();
+            }
+        }
+
+        async function sellShares() {
+            
+        }
+
         onMounted(() => {
             getCurrentShares();
         })
         return {
             historyData,
+            selectValue,
+            onlyNumber,
+            sellShares,
+            sharesNumber,
         }
     }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
+    .Btn-Confirm {
+        margin-top: 5%;
+
+    }
+    .container {
+        margin-top: 50px;
+        padding-left: 5%;
+    }
+    select {    
+
+        background-color: transparent;
+        border: none;
+        padding: 0 1em 0 0;
+        margin: 0;
+        width: 100%;
+        font-size: inherit;
+        cursor: inherit;
+        line-height: inherit;
+        height: 2em;
+    }
 </style>
